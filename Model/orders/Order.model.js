b1158eb44a7bcebb8defb5fc29c0c246f8193693
AmirHosein <awir.software@gmail.com>
2023-04-03 15:42:36 +0430

    set order model

---
+++ Model/orders/Order.model.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        date: '2023-05-05',
+        subtotal: 90.22,
+        items: [
+            {
+                product:{
+                    id: 'resshoe'
+                },
+                quantity
+            }
+        ]
+    }
+]
